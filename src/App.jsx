3eca97586e2cf9926284b36dde3d613d0907dac7
santiagoWSU <sdonadio@worcester.edu>
2025-10-06 11:13:57 -0400

    Added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./App.css";
 
 export default function App(){
     return (
 
-        <div>
+        <div className="center-container">
             <h1>Heading test</h1>
         </div>
 

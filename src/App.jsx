6f7f0b58cf957bfdf016dd023308e8c092408ea6
santiagoWSU <sdonadio@worcester.edu>
2025-10-13 20:34:15 -0400

    Successfully added ascii converter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
-export default function App(){
-    return (
+function AsciiConverter() {
 
-        <div className="center-container">
-            <h1>Changing heading</h1>
-        </div>
+    const [text, setText] = useState("HOWALIVE");
 
-    );
+    const handleChange = (event) => {
+        setText(event.target.value)
+    }
+
+    const asciiMap = new Map();
+    for(let i = 32; i <= 126; i++){
+        asciiMap.set(String.fromCharCode(i), i)
+    }
+
+    const asciiCodes = text
+        .split("")
+        .map((char) => asciiMap.get(char) || "n/a")
+        .join(" ");
+
+    return(<div>
+        <textarea
+            value={text}
+            onChange={handleChange}
+            rows={5}
+            cols={20}
+        ></textarea>
+        <p>Ascii Codes: {asciiCodes}</p>
+    </div>);
+
 }
+
+export default AsciiConverter;

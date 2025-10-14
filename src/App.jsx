5a15ff626584105cccfb5ee81c4e0e110a4aa4b1
santiagoWSU <sdonadio@worcester.edu>
2025-10-14 17:26:27 -0400

    All in main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,59 @@
 import React, {useState} from "react";
 import AsciiConverter from "./AsciiConverter";
-import { IMAGES } from "./assets/images";
 import "./App.css";
 
 
 function App(){
+
+    const [selected, setSelected] = useState(null)
+
+    const toggle = (i) => {
+        if (selected === i) {
+            return setSelected(null)
+        }
+
+        setSelected(i)
+    }
+
     return (
         <section className="hero">
-            <div className="content">
+            
+            <div className="main">
+            <div className="accordion">
+                {data.map((item, i) => (
+                    <div className="item">
+                        <div className="title" onClick={() => toggle(i)}>
+                            <h3>{item.question}</h3>
+                            <span>{selected === i? '-': '+'}</span>
+                        </div>
+                        <div className={
+                            selected === i? 'content show' : 'content'
+                        }
+                        >   
+                            {item.answer}</div>
+                    </div>
+                ))}
+            </div>
                 <h1>Inspired by <em>The Martian</em></h1>
                 <AsciiConverter/>
             </div>
         </section>
     )
 }
+
+const data = [
+    {
+        question: "Question 1",
+        answer: "This is the answer1",
+    },
+    {
+        question: "Question 2",
+        answer: "This is the answer2",
+    },
+    {
+        question: "Question 3",
+        answer: "This is the answer3",
+    },
+]
 
 export default App;

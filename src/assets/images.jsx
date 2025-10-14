900508762195740a425656c559d87c506660ea51
santiagoWSU <sdonadio@worcester.edu>
2025-10-13 21:25:06 -0400

    Fully working with images now

--- src/assets/images.jsx
+++ src/assets/images.jsx
@@ -1,6 +1,6 @@
 import asciiTable from "./asciiTable.png"
 import decoding from "./decoding.png"
-import howalive from "./howAlive.png"
+import howAlive from "./howAlive.png"
 
 export const IMAGES = {
     asciiTable,
